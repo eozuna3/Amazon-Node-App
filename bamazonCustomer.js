667969b41a5599cd9f0498d2e5d9044f4d9af780
eozuna3 <eo30239@gmail.com>
2020-04-13 20:10:39 -0500

    Created a function to display the stock of available item and to allow the user to place an order using inquirer.

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -18,5 +18,75 @@ var connection = mysql.createConnection({
 connection.connect(function(err) {
   if (err) throw err;
   console.log("The connection id is " + connection.threadId);
-  connection.end();
+  //displayAvailableItems();
+  placeOrder();
+  //connection.end();
 });
+
+function displayAvailableItems(){
+  var query = "SELECT item_id, product_name, price FROM products ";
+  connection.query(query, function(err, results) {
+    if (err) throw err;
+    console.log("\nHere is a list of available items in the store for purchases\n");
+    for (var i = 0; i < results.length; i++) {
+      console.log(results[i].item_id + ") Product Name: " + results[i].product_name + ", Price: " + results[i].price + ".");
+    }
+    console.log("-------------------------------------------------\n");
+    placeOrder();
+  });
+}
+
+function placeOrder(){
+  inquirer
+    .prompt([
+      {
+        type: "input",
+        message: "Please enter the number of the product you would like to buy.",
+        name: "productId",
+        validate: function validateUserInput(productId) {
+          var integers = /^[0-9]+$/;
+          if (productId !== "" && productId.match(integers) !== null) {
+          return true;
+          }
+        }
+      },
+      {
+        type: "input",
+        message: "Please enter the number of units of the product you would like to buy.",
+        name: "unitNumbers",
+        validate: function validateUserInput(unitNumbers) {
+          var integers = /^[0-9]+$/;
+          if (unitNumbers !== "" && unitNumbers.match(integers) !== null) {
+          return true;
+          }
+        }
+      }
+    ]).then(function (inquirerResponse){
+      console.log(inquirerResponse.productId);
+      console.log(inquirerResponse.unitNumbers);
+
+      confirmQuantity(inquirerResponse.productId, inquirerResponse.unitNumbers);
+    });
+    //connection.end();
+}
+
+function confirmQuantity(id, units){
+  var query = "SELECT stock_quantity FROM products WHERE item_id = " + id;
+  connection.query(query, function(err, results) {
+    if (err) throw err;
+    if (results.length === 0){
+      console.log("\nNo available quantities were found for the item requested\n");
+    } else if (results[0].stock_quantity < units) {
+      console.log("Sorry there is not enough product instock to fill your order.  Please try again.")
+    } else {
+      console.log("Enough stock is available.")
+      updateStock();
+    }
+    // console.log(results.length);
+    // for (var i = 0; i < results.length; i++) {
+    //   console.log(results[i].stock_quantity);
+    // }
+    console.log("-------------------------------------------------\n");
+  });
+  connection.end();
+}
